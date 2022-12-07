b19c88ffc99efdaa92458c2448facab27277a8c5
Oleg Musienko <diogen737@gmail.com>
2022-12-07 22:06:56 +0100

    [main] Basic upgrade to Next 13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { LocalizationProvider } from '@mui/lab';
-import DateAdapter from '@mui/lab/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <LocalizationProvider dateAdapter={DateAdapter}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Component {...pageProps} />
     </LocalizationProvider>
   )
