0a5938233f5ba4e35dc378af11be5d907f20ce57
Oleg Musienko <diogen737@gmail.com>
2023-01-01 13:24:28 +0100

    refactor: 💡 simplify date diff data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react'
-import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import type { NextPage } from 'next';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { Vibur } from '@next/font/google';
 
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 
 import { EventControl } from '../model/event-control';
 import { EventColor } from '../components/event-color';
 import { EventBg } from '../components/event-bg';
-import styles from '../../styles/Home.module.css'
-
+import styles from '../../styles/Home.module.css';
 
 const vibur = Vibur({ weight: '400', subsets: ['latin'] });
 
 const EventSelector = dynamic(() => import('../components/event-selector'));
 
-
 const Home: NextPage = () => {
-  const [eventDate, setEventDate] = useState(dayjs().startOf('year').add(1, 'year'));
+  const [eventDate, setEventDate] = useState(
+    dayjs().startOf('year').add(1, 'year')
+  );
   const [eventName, setEventName] = useState('New Year');
   const [eventColor, setEventColor] = useState(EventColor.Red);
   const [eventBg, setEventBg] = useState(EventBg.new_year);
-  const [diffD, setDiffD] = useState(0);
-  const [diffH, setDiffH] = useState(0);
-  const [diffM, setDiffM] = useState(0);
-  const [diffS, setDiffS] = useState(0);
+  const [timeDiff, setTimeDiff] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
   // Actual countdown logic
   const recalculateDiff = () => {
@@ -38,26 +40,25 @@ const Home: NextPage = () => {
     diff = (diff - minutes) * 60;
     const seconds = Math.floor(diff);
 
-    setDiffD(days);
-    setDiffH(hours);
-    setDiffM(minutes);
-    setDiffS(seconds);
-  }
+    setTimeDiff({ days, hours, minutes, seconds });
+  };
 
   useEffect(() => {
     recalculateDiff();
     const id = setInterval(() => recalculateDiff(), 300);
     return () => clearInterval(id);
-  })
+  });
 
   useEffect(() => {
     // December, January
-    setEventBg([0, 11].includes(eventDate.month()) ? EventBg.new_year : EventBg.regular);
+    setEventBg(
+      [0, 11].includes(eventDate.month()) ? EventBg.new_year : EventBg.regular
+    );
   }, [eventDate]);
 
   // Rendering logic
   const numberFormatter = (input: number): string => {
-    return input.toLocaleString([], { minimumIntegerDigits: 2 })
+    return input.toLocaleString([], { minimumIntegerDigits: 2 });
   };
 
   const nxModel: EventControl = {
@@ -67,11 +68,10 @@ const Home: NextPage = () => {
     setEventName,
     setEventDate,
     setEventColor,
-  }
+  };
 
   return (
     <div className={styles.container}>
-
       <div className={`${styles.cover} ${eventBg.class}`}>
         <Image
           src={eventBg.url}
@@ -80,34 +80,36 @@ const Home: NextPage = () => {
           fill
           sizes="100vw"
           style={{
-            objectFit: "cover"
-          }}>
-        </Image>
+            objectFit: 'cover',
+          }}
+        ></Image>
       </div>
 
       <EventSelector nxModel={nxModel}></EventSelector>
 
-      <main className={`${styles.main} ${styles[eventColor]} ${vibur.className}`}>
+      <main
+        className={`${styles.main} ${styles[eventColor]} ${vibur.className}`}
+      >
         <h1 className={styles.title}>
           <span className={styles.dynamic}>{eventName}</span>
           <span className={styles.static}> countdown</span>
         </h1>
 
         <div className={styles.countdownContainer}>
-          <div className={[styles.days, styles.item].join(' ')}>
-            <span className={styles.digit}>{ numberFormatter(diffD) }</span>
+          <div className={`${styles.days} ${styles.item}`}>
+            <span className={styles.digit}>{numberFormatter(timeDiff.days)}</span>
             <span className={styles.text}>Days</span>
           </div>
           <div className={[styles.hours, styles.item].join(' ')}>
-            <span className={styles.digit}>{ numberFormatter(diffH) }</span>
+            <span className={styles.digit}>{numberFormatter(timeDiff.hours)}</span>
             <span className={styles.text}>Hours</span>
           </div>
           <div className={[styles.minutes, styles.item].join(' ')}>
-            <span className={styles.digit}>{ numberFormatter(diffM) }</span>
+            <span className={styles.digit}>{numberFormatter(timeDiff.minutes)}</span>
             <span className={styles.text}>Minutes</span>
           </div>
           <div className={[styles.seconds, styles.item].join(' ')}>
-            <span className={styles.digit}>{ numberFormatter(diffS) }</span>
+            <span className={styles.digit}>{numberFormatter(timeDiff.seconds)}</span>
             <span className={styles.text}>Seconds</span>
           </div>
         </div>
@@ -127,9 +129,10 @@ const Home: NextPage = () => {
               width={72}
               height={16}
               style={{
-                maxWidth: "100%",
-                height: "auto"
-              }} />
+                maxWidth: '100%',
+                height: 'auto',
+              }}
+            />
           </span>
         </a>
       </footer>
@@ -137,6 +140,6 @@ const Home: NextPage = () => {
       <div id="modal-root"></div>
     </div>
   );
-}
+};
 
 export default Home;
