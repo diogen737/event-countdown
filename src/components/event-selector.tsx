65c0cf47813c8cc899be9b1f144693837b22cdc3
Oleg Musienko <diogen737@gmail.com>
2023-01-02 21:32:35 +0100

    feat: 🎸 modal design changes

--- src/components/event-selector.tsx
+++ src/components/event-selector.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 
 import dayjs, { Dayjs } from 'dayjs';
 import {
+  Alert,
   createTheme,
   FormControl,
   IconButton,
@@ -23,10 +24,13 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import ColorizeIcon from '@mui/icons-material/Colorize';
 import { DatePicker } from '@mui/x-date-pickers';
 
-import { EventConfig, EventType, EVENT_COLORS, EVENT_TYPES } from '@/model/event-config';
+import {
+  EventConfig,
+  EventType,
+  EVENT_COLORS,
+  EVENT_TYPES,
+} from '@/model/event-config';
 import { LS_EVENT } from '@/model/const/keys';
-
-import styles from '@/styles/DateSelector.module.css';
 
 export default function EventSelector({
   config,
@@ -41,36 +45,35 @@ export default function EventSelector({
   const [eventType, setType] = useState(config.type!);
 
   // Colorizer handler
-  const handleClickColorizer = () => {
-    const currentEntryIndex = EVENT_COLORS.findIndex(
-      (e) => e === config.color
-    );
+  const cycleColor = () => {
+    const currentEntryIndex = EVENT_COLORS.findIndex((e) => e === config.color);
     const nextEntryIndex = (currentEntryIndex + 1) % EVENT_COLORS.length;
     setState(config.setColor(EVENT_COLORS[nextEntryIndex]));
   };
 
   // Modal handlers
-  const handleClickOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const dialogOpen = () => setOpen(true);
+  const dialogClose = () => setOpen(false);
   const handleSubmit = () => {
     setState(config.setName(eventName).setDate(eventDate).setType(eventType));
     // persist config
     localStorage.setItem(LS_EVENT, config.toJSON());
-    handleClose();
+    dialogClose();
   };
 
   // Form handlers
-  const handleDateChange = (e: Dayjs | null) => {
+  const dateChange = (e: Dayjs | null) => {
     const date = e || dayjs();
     setEventDate(date);
     setType(config.getType(date));
   };
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const nameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEventName(e.target.value.trim() || '');
   };
-  const handleTypeChange = (e: SelectChangeEvent) => {
+  const typeChange = (e: SelectChangeEvent) => {
     setType(e.target.value as EventType);
   };
+
   const eventNameValidator = () => {
     if (!eventName?.trim().length) {
       return { error: true, text: 'Must not be empty' };
@@ -89,46 +92,59 @@ export default function EventSelector({
         main: '#ccc',
       },
     },
+    components: {
+      MuiAlert: {
+        styleOverrides: {
+          icon: {
+            alignItems: 'center',
+          },
+        },
+      },
+    },
   });
 
   const typeItems = Object.entries(EVENT_TYPES).map(([_, v]) => (
-    <MenuItem value={v} key={v}>{v}</MenuItem>
+    <MenuItem value={v} key={v}>
+      {v}
+    </MenuItem>
   ));
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className={styles.cog}>
-        <Box sx={{ p: 1.5 }}>
-          <IconButton
-            sx={{ p: 2 }}
-            size="large"
-            color="secondary"
-            title="Change neon color"
-            onClick={handleClickColorizer}
-          >
-            <ColorizeIcon></ColorizeIcon>
-          </IconButton>
+      <Box
+        sx={{ p: 1.5, position: 'absolute', right: 0, bottom: 0, zIndex: 2 }}
+      >
+        <IconButton
+          sx={{ p: 2 }}
+          size="large"
+          color="secondary"
+          title="Change neon color"
+          onClick={cycleColor}
+        >
+          <ColorizeIcon></ColorizeIcon>
+        </IconButton>
 
-          <IconButton
-            sx={{ p: 2 }}
-            size="large"
-            color="secondary"
-            title="Choose anoter event"
-            onClick={handleClickOpen}
-          >
-            <SettingsIcon></SettingsIcon>
-          </IconButton>
-        </Box>
-      </div>
+        <IconButton
+          sx={{ p: 2 }}
+          size="large"
+          color="secondary"
+          title="Choose anoter event"
+          onClick={dialogOpen}
+        >
+          <SettingsIcon></SettingsIcon>
+        </IconButton>
+      </Box>
 
       <Dialog
         open={open}
-        onClose={handleClose}
+        onClose={dialogClose}
         maxWidth={'xs'}
         fullWidth={true}
       >
-        <DialogTitle>Choose event&apos;s date</DialogTitle>
-        <DialogContent>
+        <DialogTitle sx={{ m: 0, px: 3, py: 1 }}>
+          Configure your event
+        </DialogTitle>
+        <DialogContent dividers>
           <Stack
             spacing={4}
             component="form"
@@ -145,7 +161,7 @@ export default function EventSelector({
               type="text"
               label="Name"
               defaultValue={eventName}
-              onChange={handleNameChange}
+              onChange={nameChange}
               margin="none"
               variant="outlined"
               error={eventNameValidator().error}
@@ -156,7 +172,7 @@ export default function EventSelector({
               label="Date"
               inputFormat="DD.MM.YYYY"
               value={eventDate}
-              onChange={handleDateChange}
+              onChange={dateChange}
               disablePast
               renderInput={(params) => <TextField {...params} />}
             />
@@ -168,16 +184,19 @@ export default function EventSelector({
                 id="demo-simple-select"
                 value={eventType}
                 label="Type"
-                onChange={handleTypeChange}
+                onChange={typeChange}
               >
                 {typeItems}
               </Select>
             </FormControl>
+
+            <Alert variant="outlined" severity="info" sx={{ fontSize: 10 }}>
+              Event will be saved permanently on this device
+            </Alert>
           </Stack>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit}>Done</Button>
+        <DialogActions sx={{ px: 3, py: 1 }}>
+          <Button onClick={handleSubmit}>Save</Button>
         </DialogActions>
       </Dialog>
     </ThemeProvider>
