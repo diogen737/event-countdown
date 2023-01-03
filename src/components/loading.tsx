6685a00721dedd067218dabf37b8703dda985649
Oleg Musienko <diogen737@gmail.com>
2023-01-03 08:39:21 +0100

    feat: 🎸 loading screen with backdrop

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,3 +1,12 @@
+import { Backdrop, CircularProgress } from '@mui/material';
+
 export default function Loading() {
-  return <div style={{ color: 'black' }}>Loading...</div>;
+  return (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={true}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
 }
